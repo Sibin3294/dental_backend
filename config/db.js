15bb514764ddbbc0e764f1711008cf99cd43f964
jivrompvt <jivrompvt@gmail.com>
2025-12-07 23:08:02 +0530

    Updated mongodb connection code

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     console.log("erfnbvnrf");
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("MongoDB Connected");
   } catch (err) {
     console.error(err);
