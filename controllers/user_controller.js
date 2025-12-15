825fc157df8a91059e7157242d52cfd16f2044c2
jivrompvt <jivrompvt@gmail.com>
2025-12-15 21:47:21 +0530

    Added profile api

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -138,3 +138,40 @@ exports.updatePatient = async (req, res) => {
     });
   }
 };
+
+// user profile
+
+exports.getUserProfile = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        message: "User ID is required"
+      });
+    }
+
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    return res.json({
+      success: true,
+      data: user
+    });
+
+  } catch (error) {
+    console.error("Get User Profile Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error"
+    });
+  }
+};
+
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -5,12 +5,14 @@ const { getAllPatients } = require("../controllers/user_controller");
 const {addMorePatientInfo}= require("../controllers/user_controller");
 const {deletePatient}= require("../controllers/user_controller");
 const {updatePatient}= require("../controllers/user_controller")
+const {getUserProfile}= require("../controllers/user_controller")
 
 const router = express.Router();
 
 router.get("/getAllPatients", getAllPatients);
 router.post("/addMorePatientInfo",addMorePatientInfo);
 router.delete("/:id",deletePatient);
-router.put("/update/:id",updatePatient)
+router.put("/update/:id",updatePatient);
+router.get("/profile/:userId",getUserProfile);
 
 module.exports = router;
