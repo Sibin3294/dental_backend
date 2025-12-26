ca2d8f7a3b110aa30deaa43b5529480ba927f37c
jivrompvt <jivrompvt@gmail.com>
2025-12-26 14:47:19 +0530

    added firebaseadmin.js file bug fixes

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,15 +1,14 @@
-// firebaseAdmin.js
-import admin from "firebase-admin";
+// config/firebaseAdmin.js
+const admin = require("firebase-admin");
 
-// Parse service account JSON from environment variable
+// Parse service account JSON from Render env variable
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// Initialize Firebase Admin only once
+// Initialize only once
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
 
-// Export admin to use in other files
-export default admin;
+module.exports = admin;
--- utils/pushNotification.js
+++ utils/pushNotification.js
@@ -1,15 +1,11 @@
-import admin from "../config/firebaseAdmin.js";
-
-
+// utils/pushNotification.js
+const admin = require("../config/firebaseAdmin");
 
 exports.sendPushToMany = async (tokens, title, body, data = {}) => {
-  if (!tokens.length) return;
+  if (!tokens || !tokens.length) return;
 
   const message = {
-    notification: {
-      title,
-      body,
-    },
+    notification: { title, body },
     data,
     tokens,
   };
